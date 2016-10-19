82f93ae0559822c343a86b7583f81d7beb829d1a
annevisje <anne.vissers@live.nl>
2016-10-19 13:30:37 +0200

    edit customers page

---
+++ public/customers.php
@@ -0,0 +1,51 @@
+<?php
+require 'header.php';
+?>
+
+<div class="container">
+    <div class="main-part col-md-9">
+        <header class="col-md-12">
+            <div class="info-bar">
+                <div class="col-md-12">
+                    <h2>Barroc-IT system</h2>
+                </div>
+                <div class="col-md-12">
+                    <h3>This is just a template</h3>
+                </div>
+            </div>
+        </header>
+        <section class="information">
+        	<div class="clients">
+        		<ul class="client col-md-12">
+        			<li class="col-md-3"><a href="#">Bedrijfsnaam</a></li>
+        			<li class="col-md-3">Telefoonnummer</li>
+        			<li class="col-md-3">Klantnaam</li>
+        			<li class="col-md-3">E-mail adress</li>
+        		</ul>
+        		<ul class="client col-md-12">
+        			<li class="col-md-3"><a href="#">Bedrijfsnaam</a></li>
+        			<li class="col-md-3">Telefoonnummer</li>
+        			<li class="col-md-3">Klantnaam</li>
+        			<li class="col-md-3">E-mail adress</li>
+        		</ul>
+        		<ul class="client col-md-12">
+        			<li class="col-md-3"><a href="#">Bedrijfsnaam</a></li>
+        			<li class="col-md-3">Telefoonnummer</li>
+        			<li class="col-md-3">Klantnaam</li>
+        			<li class="col-md-3">E-mail adress</li>
+        		</ul>
+        		<ul class="client col-md-12">
+        			<li class="col-md-3"><a href="#">Bedrijfsnaam</a></li>
+        			<li class="col-md-3">Telefoonnummer</li>
+        			<li class="col-md-3">Klantnaam</li>
+        			<li class="col-md-3">E-mail adress</li>
+        		</ul>
+        	</div>
+        </section>
+    </div>
+    <aside class="col-md-3"></aside>
+</div>
+
+<?php
+require 'footer.php';
+?>
